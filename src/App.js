9d626ad4605e2d3b54c46dd5cc6f8a2ae30baf23
bleudreams <kashi.halma@gmail>
2019-04-27 16:17:58 -0600

    basic dependencies and projects structure setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+
+class App extends React.Component{
+  render() {
+    return (
+      <View>
+        <Text>Init</Text>
+      </View>
+    );
+  }
+}
+
+
+export default App
